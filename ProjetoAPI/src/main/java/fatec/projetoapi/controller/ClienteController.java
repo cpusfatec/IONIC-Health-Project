cc8e6924d7f40c1f070ba22d6600a613b9731541
Higor-SM <higormariano10@gmail.com>
2021-11-24 10:00:16 -0300

    Melhoria visual e ajuste na edição de cadastro

--- ProjetoAPI/src/main/java/fatec/projetoapi/controller/ClienteController.java
+++ ProjetoAPI/src/main/java/fatec/projetoapi/controller/ClienteController.java
@@ -3,6 +3,8 @@ package fatec.projetoapi.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
